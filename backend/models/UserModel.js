ba92a342cf7db5e2d4743c0bc812f4191f4cfb5f
Ritik <ritiktyagi01356@gmail.com>
2023-07-13 11:45:49 +0530

    ommit

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -18,7 +18,6 @@ const userSchema = mongoose.Schema(
     },
     pic: {
       type: "String",
-      required: true,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
@@ -31,18 +30,18 @@ const userSchema = mongoose.Schema(
   { timestaps: true }
 );
 
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified) {
-//     next();
-//   }
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
 
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const {notFound , errorHandler} = require('./errorMiddleware/errorMiddleware')
+
+dotenv.config();
+connectDB();
 
 const app = express();
-dotenv.config();
+
+app.use(express.json()); // To accept Json data
 
 app.get("/", (reg, res) => {
   res.send("API is Running by ritik tyagi");
 });
 
-app.get("/api/chat", (reg, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:id", (reg, res) => {
-  // console.log(reg.params.id);
-  const singleChat = chats.find((c) => c._id === reg.params.id);
-  res.send(singleChat);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`Server started on PORT ${PORT}`));
+app.listen(5000, console.log(`Server started on PORT ${PORT}`.yellow.bold));
